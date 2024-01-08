6ff32cd35ae3d090bbbfccd64a66edb0acbbac4a
avasilenko <snakevil@mail.ru>
2024-01-08 20:06:58 +0300

    task2 done

---
+++ task2/Program.cs
@@ -0,0 +1,27 @@
+﻿int x = -8;
+int y = 0;
+if(x==0 ^ y==0)
+    Console.WriteLine("нельзья вводить 0");
+else if (x>0 && y>0)
+    Console.WriteLine(" I квадрат");
+else if (x<0 && y<0)
+    Console.WriteLine(" III квадрат");
+else if (x<0 && y>0)
+    Console.WriteLine(" II квадрат");
+else 
+  Console.WriteLine(" IV квадрат");
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
