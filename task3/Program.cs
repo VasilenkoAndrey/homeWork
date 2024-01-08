22ee767177e5adc3187e89f28f10e212519a2a56
avasilenko <snakevil@mail.ru>
2024-01-08 20:18:43 +0300

    task3 done

---
+++ task3/Program.cs
@@ -0,0 +1,12 @@
+﻿int num = 10;
+int first = num / 10;
+int second = num % 10;
+    if(first > second)
+      Console.WriteLine(first + " является большей цифрой.");
+    else if(second > first)
+      Console.WriteLine(second + " является большей цифрой.");
+    else
+      Console.WriteLine("Обе цифры равны по величине.");
+
+
+    
