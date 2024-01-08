77797ddd3a70b65795252dbf827cb98ec8cd4fb8
avasilenko <snakevil@mail.ru>
2024-01-08 11:18:19 +0300

    task1_done

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int n = 161;
+if (n % 7 == 0 && n % 23 == 0)
+  Console.WriteLine($"Число {n} кратно 7 и 23");
+else
+  Console.WriteLine($"Число {n} не кратно 7 или 23, или обоим числам");
+
+
